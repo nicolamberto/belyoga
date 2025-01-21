c3f5b75ecea6a2e073c80e43b1153958eb3933b5
Nicolas Lamberto <nicolaslamberto2840@gmail.com>
2025-01-21 19:45:57 -0300

    again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,20 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+      requireConfigFile: false,
+      babelOptions: {
+        presets: ['@babel/preset-react'],
+      },
+    },
+    rules: {
+      // Tus reglas personalizadas
+    },
+  },
+];
 
 export default eslintConfig;
