9fd2abec868891309e398688ac0e6c76a5900693
Nicolas Lamberto <nicolaslamberto2840@gmail.com>
2025-03-13 19:01:21 -0300

    eliminando por completo leaflet

--- src/app/components/contact/map/dynamicmap.jsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import dynamic from 'next/dynamic';
-
-const DynamicMap = dynamic(() => import('./map'), {
-    ssr: false // Desactiva la renderización del lado del servidor
-});
-
-export default DynamicMap;
